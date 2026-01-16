bcfe3210a9cf6b7fdd96fc321b1e2081b44394cf
Arsyad Hamidi <arsyadhamidi04@gmail.com>
2026-01-16 18:13:46 +0700

    commit

--- app/Http/Controllers/Dashboard/DashboardController.php
+++ app/Http/Controllers/Dashboard/DashboardController.php
@@ -12,7 +12,7 @@ use Illuminate\Support\Facades\DB;
 
 class DashboardController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
         $countInstansis = Instansi::count();
         $countJaringans = Jaringan::count();
@@ -38,6 +38,68 @@ class DashboardController extends Controller
             ->orderBy('bulan')
             ->pluck('total', 'bulan')
             ->toArray();
+
+        if ($request->ajax()) {
+            $perPage = $request->input('length', 10);
+            $search = $request->input('search', '');
+
+            $query = LaporanGangguan::join('instansis', 'laporan_gangguans.instansi_id', 'instansis.id')
+                ->join('jaringans', 'laporan_gangguans.jaringan_id', 'jaringans.id')
+                ->join('status_laporans', 'laporan_gangguans.status_id', 'status_laporans.id')
+                ->join('users', 'laporan_gangguans.users_id', 'users.id')
+                ->select([
+                    'laporan_gangguans.id',
+                    'laporan_gangguans.status_id',
+                    'laporan_gangguans.judul',
+                    'laporan_gangguans.deskripsi',
+                    'laporan_gangguans.waktu_kejadian',
+                    'laporan_gangguans.prioritas',
+                    'instansis.nm_instansi',
+                    'jaringans.tipe_jaringan',
+                    'jaringans.provider',
+                    'jaringans.ip_address',
+                    'jaringans.bandwidth',
+                    'jaringans.status',
+                    'jaringans.keterangan',
+                    'status_laporans.nm_status',
+                    'status_laporans.warna',
+                    'users.name',
+                ])
+                ->where('laporan_gangguans.status_id', '1')
+                ->orderBy('laporan_gangguans.id', 'desc');
+
+            if ($search) {
+                $query->where(function ($query) use ($search) {
+                    $query->Where('users.name', 'LIKE', "%{$search}%")
+                        ->orWhere('instansis.nm_instansi', 'LIKE', "%{$search}%");
+                });
+            }
+
+            $totalRecords = $query->count(); // Hitung total data
+
+            $data = $query->paginate($perPage); // Gunakan paginate() untuk membagi data sesuai dengan halaman dan jumlah per halaman
+
+            // Tambahkan kolom aksi
+            $dataWithActions = $data->map(function ($item) {
+                $tindakUrl = route('admin-tindaklanjut.tindaklanjut', $item->id ?? '');
+
+                $item->aksi = '
+        <a href="' . $tindakUrl . '" class="btn btn-outline-primary me-1">
+            <i class="fas fa-reply"></i>
+        </a>
+    ';
+
+                return $item;
+            });
+
+
+            return response()->json([
+                'draw' => $request->input('draw'), // Ambil nomor draw dari permintaan
+                'recordsTotal' => $totalRecords, // Kirim jumlah total data
+                'recordsFiltered' => $totalRecords, // Jumlah data yang difilter sama dengan jumlah total
+                'data' => $dataWithActions, // Kirim data yang sesuai dengan halaman dan jumlah per halaman
+            ]);
+        }
 
         return view('dashboard.main.index', [
             'countInstansis' => $countInstansis,
--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -79,6 +79,37 @@
 </div>
 
 <div class="row mt-4">
+    <div class="col-lg-12">
+        <div class="card">
+            <div class="card-header">
+                Laporan Gangguan
+            </div>
+            <div class="card-body table-responsive">
+                <table class="table table-bordered table-striped"
+                       id="myTable"
+                       style="width: 100%">
+                    <thead>
+                        <tr>
+                            <th style="width: 4%">No.</th>
+                            <th>Petugas</th>
+                            <th>Instansi</th>
+                            <th>Jaringan</th>
+                            <th>Judul</th>
+                            <th>Deskripsi</th>
+                            <th>Prioritas</th>
+                            <th>Waktu</th>
+                            <th>Status</th>
+                            <th>Aksi</th>
+                        </tr>
+                    </thead>
+                    <tbody></tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="row mt-4">
     <div class="col-md-12">
         <div class="card card-primary">
             <div class="card-header">
@@ -121,6 +152,148 @@
 
 
 @push('custom-script')
+    <script>
+        $(document).ready(function() {
+            $.ajaxSetup({
+                headers: {
+                    "X-CSRF-TOKEN": $('meta[name="csrf-token"]').attr("content"),
+                },
+            });
+
+            $('#selectedStatus').select2({
+                theme: 'bootstrap4'
+            });
+
+            var startDate = moment().startOf('month');
+            var endOfDate = moment().endOf('month');
+
+            $('#searchByDate').daterangepicker({
+                startDate: startDate,
+                endDate: endOfDate,
+                locale: {
+                    format: 'YYYY-MM-DD',
+                    applyLabel: 'Terapkan',
+                    cancelLabel: 'Batal',
+                    customRangeLabel: 'Pilih Rentang Tanggal',
+                    daysOfWeek: [
+                        'Min', 'Sen', 'Sel', 'Rab', 'Kam', 'Jum', 'Sab'
+                    ],
+                    monthNames: [
+                        'Januari', 'Februari', 'Maret', 'April', 'Mei', 'Juni',
+                        'Juli', 'Agustus', 'September', 'Oktober', 'November', 'Desember'
+                    ],
+                    firstDay: 1
+                },
+                ranges: {
+                    'Hari Ini': [moment(), moment()],
+                    'Kemarin': [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
+                    '7 Hari Terakhir': [moment().subtract(6, 'days'), moment()],
+                    '30 Hari Terakhir': [moment().subtract(29, 'days'), moment()],
+                    'Bulan Ini': [moment().startOf('month'), moment().endOf('month')],
+                    'Bulan Lalu': [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')]
+                }
+            }, function(start_date, end_date) {
+                myTable.draw();
+            });
+
+            // Tampilkan Data
+            let myTable = $("#myTable").DataTable({
+                processing: true,
+                serverSide: true,
+                paging: true,
+                pageLength: 10,
+                lengthMenu: [
+                    [10, 25, 50, 100, 250],
+                    [10, 25, 50, 100, 250],
+                ],
+                language: {
+                    paginate: {
+                        previous: "Sebelumnya",
+                        next: "Selanjutnya",
+                    },
+                },
+                ajax: {
+                    url: "{{ route('dashboard.index') }}",
+                    data: function(data) {
+                        data.page = Math.ceil(data.start / data.length) + 1;
+                        data.search = $("#myTable_filter input").val();
+                    },
+                },
+                columns: [{
+                        data: null,
+                        render: function(data, type, row, meta) {
+                            return meta.row + meta.settings._iDisplayStart + 1;
+                        },
+                        orderable: false,
+                    },
+                    {
+                        data: "name",
+                        name: "name",
+                        defaultContent: "-",
+                    },
+                    {
+                        data: "nm_instansi",
+                        name: "nm_instansi",
+                        defaultContent: "-",
+                    },
+                    {
+                        data: "tipe_jaringan",
+                        name: "tipe_jaringan",
+                        defaultContent: "-",
+                    },
+                    {
+                        data: "judul",
+                        name: "judul",
+                        defaultContent: "-",
+                    },
+                    {
+                        data: "deskripsi",
+                        name: "deskripsi",
+                        defaultContent: "-",
+                    },
+                    {
+                        data: "prioritas",
+                        name: "prioritas",
+                        defaultContent: "-",
+                    },
+                    {
+                        data: "waktu_kejadian",
+                        name: "waktu_kejadian",
+                        defaultContent: "-",
+                    },
+                    {
+                        data: "warna",
+                        name: "warna",
+                        defaultContent: "-",
+                        render: function(data, type, row) {
+                            var namaStatus = row.nm_status;
+                            if (data == '1') {
+                                return '<span class="badge badge-primary">' + namaStatus + '</span>';
+                            } else if (data == '2') {
+                                return '<span class="badge badge-warning">' + namaStatus + '</span>';
+                            } else if (data == '3') {
+                                return '<span class="badge badge-success">' + namaStatus + '</span>';
+                            } else if (data == '0') {
+                                return '<span class="badge badge-danger">' + namaStatus + '</span>';
+                            }
+                            return data ?? '-';
+                        }
+                    },
+                    {
+                        data: "aksi",
+                        name: "aksi",
+                        orderable: false,
+                        searchable: false,
+                        defaultContent: "-",
+                    },
+                ],
+
+                order: [
+                    [1, "desc"]
+                ],
+            });
+        });
+    </script>
     <script>
         document.addEventListener("DOMContentLoaded", function() {
             const ctx = document.getElementById('laporanChart').getContext('2d');
